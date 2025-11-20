f3332059eca60b0136c331c1e9c4f13a9d73eff8
JuliRV <julireguei@gmail.com>
2025-11-20 12:39:38 +0100

    Initial Commit

---
+++ BarcodeScannerSwiftTests/BarcodeScannerSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerSwiftTests.swift
+//  BarcodeScannerSwiftTests
+//
+//  Created by Julian Regueira on 20/11/25.
+//
+
+import Testing
+@testable import BarcodeScannerSwift
+
+struct BarcodeScannerSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
